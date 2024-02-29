494ffeb64f4694ac7625bd742c763966a0b05350
Maria Sharkova <moriko.tys@gmail.com>
2024-02-29 17:40:15 +0300

    views

--- pickstarter/api/views.py
+++ pickstarter/api/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from collects.models import Collect, Payment
+
+from rest_framework import viewsets
+
+from .serializers import (
+    CollectSerializer, CollectListSerializer, PaymentSerializer
+)
+
+
+class CollectViewSet(viewsets.ModelViewSet):
+    queryset = Collect.objects.all()
+    serializer_class = CollectSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CollectListSerializer
+        return CollectSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CollectViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(CollectViewSet, self).perform_destroy(serializer)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+
+    def get_queryset(self):
+        collect_id = self.kwargs.get('collect_id')
+        new_queryset = Payment.objects.filter(collect=collect_id)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        collect_id = self.kwargs.get('collect_id')
+        collect = get_object_or_404(Collect, id=collect_id)
+        serializer.save(author=self.request.user, collect=collect)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PaymentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(PaymentViewSet, self).perform_destroy(serializer)
