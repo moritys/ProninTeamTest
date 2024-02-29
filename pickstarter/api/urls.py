48f7c63b445ef926e618d492a89f04014304d070
Maria Sharkova <moriko.tys@gmail.com>
2024-02-29 17:41:23 +0300

    urls

--- pickstarter/api/urls.py
+++ pickstarter/api/urls.py
@@ -3,16 +3,16 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-# from .views import
+from .views import CollectViewSet, PaymentViewSet
 
 
 router_v1 = DefaultRouter()
-# router_v1.register(r'^collects', CollectViewSet, basename='api_collects')
-# router_v1.register(
-#     r'^collects/(?P<collect_id>\d+)/payments',
-#     PaymentViewSet,
-#     basename='api_payments'
-# )
+router_v1.register(r'^collects', CollectViewSet, basename='api_collects')
+router_v1.register(
+    r'^collects/(?P<collect_id>\d+)/payments',
+    PaymentViewSet,
+    basename='api_payments'
+)
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
